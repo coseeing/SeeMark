c12912f40b453a0d8982e25447485ccf5d550fb8
Wendell Liu <cuk.bas@gmail.com>
2025-06-17 22:29:03 +0800

    feat: dummy test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginJest from 'eslint-plugin-jest';
 
 export default defineConfig([
   {
@@ -19,4 +20,19 @@ export default defineConfig([
     },
   },
   eslintPluginPrettierRecommended,
+  {
+    // update this to match your test files
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
 ]);
