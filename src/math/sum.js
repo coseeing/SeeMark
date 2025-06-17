cb93bb2589724e7b6ee8c754b6f2a6a3cf2b7c4d
Wendell Liu <cuk.bas@gmail.com>
2025-06-17 22:30:01 +0800

    feat: remove the validation

--- src/math/sum.js
+++ src/math/sum.js
@@ -1,8 +1,4 @@
 const sum = (a, b) => {
-  // throw an error if inputs are not numbers
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new TypeError('Both arguments must be numbers');
-  }
   return a + b;
 };
 
