7e70de2ce595170c43e49241ec986aa543ba9590
Wendell Liu <cuk.bas@gmail.com>
2025-07-28 20:50:02 +0800

    chore: mv

--- src/markup-converters/react/default-components/default-components.js
+++ src/markup-converters/react/default-components/default-components.js
@@ -1,6 +1,6 @@
 import { SUPPORTED_COMPONENT_TYPES } from '../../../shared/supported-components';
 
-import Alert from './Alert';
+import Alert from './alert/Alert';
 
 const defaultComponents = { [SUPPORTED_COMPONENT_TYPES.ALERT]: Alert };
 
