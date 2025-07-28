937aa54cd9784e1253e47ab74e859e8859e4b428
Wendell Liu <cuk.bas@gmail.com>
2025-07-28 20:47:57 +0800

    test: rendering

--- src/markup-converters/react/converter.test.js
+++ src/markup-converters/react/converter.test.js
@@ -1,6 +1,10 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
+import {
+  SEE_MARK_PAYLOAD_DATA_ATTRIBUTES,
+  SEEMARK_ELEMENT_TYPE_DATA_ATTRIBUTE,
+} from '../../shared/common-markup';
 import converter from './converter';
 
 describe('Converter', () => {
@@ -14,5 +18,32 @@ describe('Converter', () => {
     expect(
       screen.getByRole('heading', { level: 1, name: 'Heading 1' })
     ).toBeInTheDocument();
+  });
+
+  it('should handle custom components', () => {
+    const elementType = 'custom-component';
+    const payload = { text: 'Custom Component Text' };
+
+    const html = `<div ${SEEMARK_ELEMENT_TYPE_DATA_ATTRIBUTE}="${elementType}" ${SEE_MARK_PAYLOAD_DATA_ATTRIBUTES}='${JSON.stringify(payload)}'><div>
+
+</div></div>`;
+    const CustomComponent = ({ text }) => (
+      <div className="customComponentWrapper">
+        <p className="customComponentText">{text}</p>
+      </div>
+    );
+
+    render(
+      converter(html, {
+        [elementType]: CustomComponent,
+      })
+    );
+
+    const textElement = screen.getByText(payload.text);
+    const wrapper = textElement.closest('div');
+
+    expect(wrapper).toHaveClass('customComponentWrapper');
+    expect(textElement.tagName).toBe('P');
+    expect(textElement).toHaveClass('customComponentText');
   });
 });
--- src/markup-converters/react/default-components/Alert.jsx
+++ src/markup-converters/react/default-components/Alert.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const Alert = ({ children, ...props }) => {
-  console.log('Alert component props:', props);
-
   return <div className="alert">{children}</div>;
 };
 
