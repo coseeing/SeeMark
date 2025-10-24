a419665ff8a5c4587a52cf4857e4275fad7e1323
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2025-10-24 22:24:39 +0800

    feat: add default components for newly added embeds

---
+++ src/markup-converters/react/default-components/codepen/CodePen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CodePen = ({ title = '', source = '' }) => {
+  // adpated from CodePen official embed
+  return (
+    <iframe
+      height="300"
+      style={{ width: '100%' }}
+      scrolling="no"
+      title={title}
+      src={source}
+      frameBorder="no"
+      loading="lazy"
+      // eslint-disable-next-line react/no-unknown-property
+      allowTransparency="true"
+    />
+  );
+};
+
+CodePen.propTypes = {
+  title: PropTypes.string,
+  source: PropTypes.string,
+};
+
+export default CodePen;
--- src/markup-converters/react/default-components/default-components.js
+++ src/markup-converters/react/default-components/default-components.js
@@ -4,12 +4,18 @@ import Alert from './alert/Alert.jsx';
 import InternalLink from './internal-link/InternalLink.jsx';
 import InternalLinkTitle from './internal-link-title/InternalLinkTitle.jsx';
 import Image from './image/Image.jsx';
+import YouTube from './youtube/YouTube.jsx';
+import CodePen from './codepen/CodePen.jsx';
+import GitHub from './github/GitHub.jsx';
 
 const defaultComponents = {
   [SUPPORTED_COMPONENT_TYPES.ALERT]: Alert,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK]: InternalLink,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK_TITLE]: InternalLinkTitle,
   [SUPPORTED_COMPONENT_TYPES.IMAGE]: Image,
+  [SUPPORTED_COMPONENT_TYPES.YOUTUBE]: YouTube,
+  [SUPPORTED_COMPONENT_TYPES.CODEPEN]: CodePen,
+  [SUPPORTED_COMPONENT_TYPES.GITHUB]: GitHub,
 };
 
 export default defaultComponents;
