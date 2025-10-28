53a896d3fbc8fc08ac5877b4f83bf53397648583
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2025-10-28 22:16:57 +0800

    feat: support external_link_title custom syntax

--- src/shared/supported-components.js
+++ src/shared/supported-components.js
@@ -4,4 +4,5 @@ export const SUPPORTED_COMPONENT_TYPES = {
   INTERNAL_LINK_TITLE: 'internalLinkTitle',
   IMAGE: 'image',
   EXTERNAL_LINK_TAB: 'externalLinkTab',
+  EXTERNAL_LINK_TITLE: 'externalLinkTitle',
 };
