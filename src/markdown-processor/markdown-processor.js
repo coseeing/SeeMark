0f2d4b3ab82d0980a119e6514f4ff91d22c4b320
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2025-10-28 21:45:02 +0800

    feat: support external_link_tab custom syntax

--- src/markdown-processor/markdown-processor.js
+++ src/markdown-processor/markdown-processor.js
@@ -4,6 +4,7 @@ import alert from './marked-extentions/alert';
 import internalLink from './marked-extentions/internal-link';
 import image from './marked-extentions/image';
 import internalLinkTitle from './marked-extentions/internal-link-title';
+import externalLinkTab from './marked-extentions/external-link-tab';
 
 const markdownProcessor = (markdownContent = '', options = {}) => {
   const asciimathDelimiter = 'graveaccent';
@@ -14,7 +15,13 @@ const markdownProcessor = (markdownContent = '', options = {}) => {
     documentFormat: options.documentFormat,
     imageFiles: options.imageFiles,
     shouldBuildImageObjectURL: options.shouldBuildImageObjectURL,
-    extensions: [alert, internalLink, internalLinkTitle, image],
+    extensions: [
+      alert,
+      internalLink,
+      internalLinkTitle,
+      image,
+      externalLinkTab,
+    ],
   });
 
   return markdownProcessor(markdownContent);
--- src/markup-converters/react/default-components/default-components.js
+++ src/markup-converters/react/default-components/default-components.js
@@ -4,12 +4,14 @@ import Alert from './alert/Alert.jsx';
 import InternalLink from './internal-link/InternalLink.jsx';
 import InternalLinkTitle from './internal-link-title/InternalLinkTitle.jsx';
 import Image from './image/Image.jsx';
+import ExternalLinkTab from './external-link-tab/ExternalLinkTab.jsx';
 
 const defaultComponents = {
   [SUPPORTED_COMPONENT_TYPES.ALERT]: Alert,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK]: InternalLink,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK_TITLE]: InternalLinkTitle,
   [SUPPORTED_COMPONENT_TYPES.IMAGE]: Image,
+  [SUPPORTED_COMPONENT_TYPES.EXTERNAL_LINK_TAB]: ExternalLinkTab,
 };
 
 export default defaultComponents;
