10e28e3758ec920d137116b2734a2f4f628b8979
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2025-12-09 09:24:10 +0800

    chore: add comment to explain regex composition

--- src/markdown-processor/marked-extentions/internal-link.js
+++ src/markdown-processor/marked-extentions/internal-link.js
@@ -1,6 +1,23 @@
 import { SUPPORTED_COMPONENT_TYPES } from '../../shared/supported-components';
 import { createRenderer } from './helpers';
 
+/**
+ * Matches internal link syntax: [display]<target>
+ *
+ * Pattern breakdown:
+ * - ^           : Must start at beginning (required by marked.js tokenizer since it checks from start of string)
+ * - \[          : Opening square bracket (escaped)
+ * - ([^\]]+)    : Capture group 1 - display text (one or more non-closing-bracket chars)
+ * - \]          : Closing square bracket (escaped)
+ * - <           : Opening angle bracket
+ * - ([^>]+)     : Capture group 2 - internal target (one or more non-closing-angle-bracket chars)
+ * - >           : Closing angle bracket
+ *
+ * Example match: [Home Page]<home>
+ * - match[1] = "Home Page" (display)
+ * - match[2] = "home" (target)
+ */
+// Regex exported for test
 export const INTERNAL_LINK_REGEXP = /^\[([^\]]+)\]<([^>]+)>/;
 
 const markedInternalLink = () => {
