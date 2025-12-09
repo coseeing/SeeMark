5e04c114fe3606ef16be3545ec097f0f2fd528ae
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2025-12-09 09:48:10 +0800

    chore: add comment to denote regex compsition

--- src/markdown-processor/marked-extentions/image-link.js
+++ src/markdown-processor/marked-extentions/image-link.js
@@ -2,6 +2,27 @@ import { SUPPORTED_COMPONENT_TYPES } from '../../shared/supported-components';
 
 import { buildHTMLMarkup } from './helpers';
 
+/**
+ * Matches image with link syntax: ![alt](imageId)((target))
+ *
+ * Pattern breakdown:
+ * - ^           : Must start at beginning (required by marked.js tokenizer since it checks from start of string)
+ * - !           : Exclamation mark (distinguishes from link syntax)
+ * - \[          : Opening square bracket (escaped)
+ * - ([^\]]*)    : Capture group 1 - alt text (zero or more non-closing-bracket chars)
+ * - \]          : Closing square bracket (escaped)
+ * - \(          : Opening parenthesis (escaped)
+ * - ([^)]+)     : Capture group 2 - image ID (one or more non-closing-paren chars)
+ * - \)          : Closing parenthesis (escaped)
+ * - \(\(        : Opening double parentheses (escaped)
+ * - ([^)]+)     : Capture group 3 - target link (one or more non-closing-paren chars)
+ * - \)\)        : Closing double parentheses (escaped)
+ *
+ * Example match: ![Thumbnail](thumb-123)((article-page))
+ * - match[1] = "Thumbnail" (alt)
+ * - match[2] = "thumb-123" (imageId)
+ * - match[3] = "article-page" (target)
+ */
 // Regex exported for test
 export const IMAGE_LINK_REGEXP = /^!\[([^\]]*)\]\(([^)]+)\)\(\(([^)]+)\)\)/;
 
