e049548866bddf42f4fa9571d11e69c7d90532fa
jamliaoo <tzuhualiao@gmail.com>
2025-12-30 22:22:28 +0800

    feat: add getAllElementsByType helper function
    * Introduced a new helper function, getAllElementsByType, to retrieve all elements of a specified type from a container. This enhances the utility of the testing helpers by allowing for more comprehensive queries in tests.

--- src/testing-helpers/custom-query.js
+++ src/testing-helpers/custom-query.js
@@ -4,3 +4,10 @@ export const getElementByType = (container, elementType) =>
   container.querySelector(
     `[${SEEMARK_ELEMENT_TYPE_DATA_ATTRIBUTE}="${elementType}"]`
   );
+
+export const getAllElementsByType = (container, elementType) =>
+  Array.from(
+    container.querySelectorAll(
+      `[${SEEMARK_ELEMENT_TYPE_DATA_ATTRIBUTE}="${elementType}"]`
+    )
+  );
