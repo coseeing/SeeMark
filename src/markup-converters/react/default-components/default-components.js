807eebcf564f3647342a913dee31bf4993d509dd
jamliaoo <tzuhualiao@gmail.com>
2025-12-30 22:22:36 +0800

    feat: add Heading component to default components
    * Introduced a new Heading component to the default components in the markdown processor, allowing for customizable heading elements.
    * Updated the defaultComponents mapping to include the new HEADING type, enhancing support for various heading levels.

--- src/markup-converters/react/default-components/default-components.js
+++ src/markup-converters/react/default-components/default-components.js
@@ -1,6 +1,7 @@
 import { SUPPORTED_COMPONENT_TYPES } from '../../../shared/supported-components';
 
 import Alert from './alert/Alert.jsx';
+import Heading from './heading/Heading.jsx';
 import InternalLink from './internal-link/InternalLink.jsx';
 import InternalLinkTitle from './internal-link-title/InternalLinkTitle.jsx';
 import Image from './image/Image.jsx';
@@ -16,6 +17,7 @@ import Iframe from './iframe/Iframe.jsx';
 
 const defaultComponents = {
   [SUPPORTED_COMPONENT_TYPES.ALERT]: Alert,
+  [SUPPORTED_COMPONENT_TYPES.HEADING]: Heading,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK]: InternalLink,
   [SUPPORTED_COMPONENT_TYPES.INTERNAL_LINK_TITLE]: InternalLinkTitle,
   [SUPPORTED_COMPONENT_TYPES.IMAGE]: Image,
